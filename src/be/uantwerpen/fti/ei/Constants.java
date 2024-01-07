e6801dfcfbd82ff616ff4ccbf89b8e87582eceaf
daerbevoli <sameer.baruwal@student.uantwerpen.be>
2024-01-07 17:35:28 +0100

    initial commit

---
+++ src/be/uantwerpen/fti/ei/Constants.java
@@ -0,0 +1,16 @@
+package be.uantwerpen.fti.ei;
+
+public interface Constants {
+
+    // general
+    int SCREENWIDTH = 500;
+    int SCREENHEIGHT = 500;
+
+    // button
+    int bWidth = 100;
+    int bHeight = 30;
+
+    // back button
+    int bbX = 10;
+    int bbY = 10;
+}
